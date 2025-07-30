c3208d7590808c6fc6a987b2a7ead7b533313de1
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-07-30 19:46:03 +0530

    Save

--- app/src/main/java/com/mdkasims/jcsimpleapptoggle/views/CarList.kt
+++ app/src/main/java/com/mdkasims/jcsimpleapptoggle/views/CarList.kt
@@ -4,9 +4,6 @@ import android.util.Log
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.mdkasims.jcsimpleapptoggle.viewmodels.CarListViewModel
 import androidx.compose.foundation.lazy.items
